5278afcb117188c49827c07d64d8f81b4d865bbc
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-03-11 20:31:32 -0300

    Updates

---
+++ precache-manifest.559e7ce195a33f193dcd30a49af89827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "351b9ffcf72cb5e8f20d",
+    "url": "/static/css/main.deec0ef9.chunk.css"
+  },
+  {
+    "revision": "351b9ffcf72cb5e8f20d",
+    "url": "/static/js/main.7c268097.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a41a29b1d51b245d4b307caf47e546a4",
+    "url": "/index.html"
+  }
+];
